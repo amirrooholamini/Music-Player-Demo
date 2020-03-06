c053cb0102ae965182398b09a29b8708feca35fd
Amir Rooholamini <amirrooholamini@gmail.com>
2020-03-06 20:48:06 +0330

    commi 2

--- app/src/main/java/custom/MohsenTitle.java
+++ app/src/main/java/custom/MohsenTitle.java
@@ -79,7 +79,7 @@ public class MohsenTitle extends LinearLayout {
         paint.setTypeface(App.mohsenTitleFont);
         //paint.setShadowLayer(1, 2, 2, Color.parseColor("#000000"));
 
-
+        // git
             canvas.drawText(name, width / 2, height / 2 - (paint.ascent() + paint.descent()) / 2, paint);
         if (moveAlpha > 0) {
             paint.setAlpha(moveAlpha);
